5b3edf6cb64063ddc1d77a10b09b8ca552de1b0b
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-12-01 23:11:40 +0530

    endpoint updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.status(200).json({name: "Bunnu"})
+    res.status(200).json({message:"Hello Bunnu"})
 })
 
 app.listen(3000)
