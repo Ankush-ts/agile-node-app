baf5851b816225cc7f7f4df96afe119ea9926dfa
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-12-01 23:08:51 +0530

    Res added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.status(200).json({name: "agile-node-app"})
+    res.status(200).json({name: "Bunnu"})
 })
 
 app.listen(3000)
